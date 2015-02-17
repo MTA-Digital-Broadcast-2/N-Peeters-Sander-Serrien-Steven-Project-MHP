5855ce98f1cf2e5e3e00f10e99331ceff221a4db
Sander Peeters <peeters94@hotmail.com>
2015-02-17 09:26:38 +0100

    no message

---
+++ SanderPeeters/blz17/EersteProg.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+
+/**
+ * De klasse EersteProg is een Java applicatie
+ *
+ * @author Sander Peeters
+ * @version 1.0
+ */
+
+public class EersteProg {
+    /**
+     *  Dit is een main-functie, hier start het programma
+     *  @param args Dit is een array van strings waarmee parameters
+     kunnen meegegeven worden vanaf de commandline.
+     */
+
+    public static void main(String args[] )
+    {
+        System.out.println(~10);
+        // blz 17
+        // om in 2's complement van 10 naar -10 te gaan
+        // kan je alle bits inverteren en dan +1
+        // maar de +1 doe je hier niet, dus je komt op -11 uit.
+        
+    }
+}
