72f9f2ffd7a40b3e08eb2f4211684e98dccdba2d
StevenSerrien <steven.serrien@student.kdg.be>
2015-04-29 14:49:27 +0200

    Waarom wordt niet alles verwijderd van de scene?

--- Project/DB-Project/src/hellotvxlet/HelloTVXlet.java
+++ Project/DB-Project/src/hellotvxlet/HelloTVXlet.java
@@ -26,11 +26,16 @@ public class HelloTVXlet implements Xlet, HActionListener {
         System.out.println(titelString);
         
         if(titelString == "startQuiz"){
-            System.out.println("DANSEEEEN");
+           System.out.println("DANSEEEEN");
+           
         }
         else
         {
             System.out.println("Geen feestje sorry");
+            scene.remove(titelLabel);
+            scene.remove(knop2);
+            scene.remove(knop1);
+            
         }
         
         
@@ -102,66 +107,7 @@ public class HelloTVXlet implements Xlet, HActionListener {
         this.actueleXletContext = context;
         //Template aanmaken
         displayTitleScreen();
-       /* HSceneTemplate sceneTemplate = new HSceneTemplate();
-        
-        //Grootte en positie
-        sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_DIMENSION, new HScreenDimension(1.0f, 1.0f), HSceneTemplate.REQUIRED);
-        sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_LOCATION, new HScreenPoint(0.0f, 0.0f), HSceneTemplate.REQUIRED);
-        
-        scene = HSceneFactory.getInstance().getBestScene(sceneTemplate);
-            
-            knop1 = new HTextButton("KNOP1");
-            knop1.setLocation(100,100);
-            knop1.setSize(100,50);
-            knop1.setBackground(new DVBColor(0,0,0,179));
-            knop1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            
-        
-        
-            knop2 = new HTextButton("KNOP2");
-            knop2.setLocation(100,200);
-            knop2.setSize(100,50);
-            knop2.setBackground(new DVBColor(0,0,0,179));
-            knop2.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            
-        knop1.setFocusTraversal(null, knop2, null, null); // Op, neer, links, rechts
-        knop2.setFocusTraversal(knop1, null, null, null);
-            
-        
-        scene.add(knop1);
-        scene.add(knop2);
-        
-        knop1.requestFocus();
-        
-        knop1.setActionCommand("knop1_actioned");
-        knop2.setActionCommand("knop2_actioned");
-        knop1.setActionCommand("knop1_actioned");
-        knop2.setActionCommand("knop2_actioned");
-        
        
-        
-        
-        
-        
-   
-        tekstLabelM = new HStaticText("Wie wordt er multimilionair?");
-        tekstLabelM.setLocation(100,-100);
-        tekstLabelM.setSize(400,250);
-        
-        scene.add(tekstLabelM);
-        
-        
-        
-        
-        tekstLabel = new HStaticText("");
-        
-        tekstLabel.setLocation(100, 100);
-        tekstLabel.setSize(400,250);
-        tekstLabel.setBackground(new DVBColor(255,255,255,179));
-        tekstLabel.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        
-        scene.add(tekstLabel);
-         */
         
     }
 
